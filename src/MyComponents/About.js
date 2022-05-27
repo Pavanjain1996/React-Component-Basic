d007f7d628435fd6e78a839f2d705654e0371b95
Pavan Jain <013pavanjain@gmail.com>
2022-05-27 17:48:28 +0530

    v1.0.3

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='container' style={{padding:'30px'}}>
+        <h2 style={{borderBottom:'3px solid red'}}>This is the about page</h2>
+        <p>This is a todo application created in react by Pavan Jain</p>
+    </div>
+  )
+}
