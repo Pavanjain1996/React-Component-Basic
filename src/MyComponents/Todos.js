706ed7d0277891a97f67a3a5f6ee2fd2819d55fa
Pavan Jain <013pavanjain@gmail.com>
2022-05-29 14:26:34 +0530

    v2.1.1

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -5,23 +5,23 @@ import AddTodo from './AddTodo'
 export default function Todos(props) {
     return (
         <div className="container" style={{ padding: '20px' }}>
-            <div className='row'>
-                <div className='col-md-6'>
-                    <div className='row' style={{ padding: '10px' }}>
+            <div className='row' style={{ padding: '20px' }}>
+                <div className='col-md-6' style={{  padding: "20px" }}>
+                    <div className='row' style={{ border: "3px solid red", padding: '10px' }}>
                         <h3>You can add tasks here for today</h3>
+                        <AddTodo addTodo={props.addTodo} />
                     </div>
-                    <AddTodo addTodo={props.addTodo} />
                 </div>
-                <div className='col-md-6'>
-                    <div className='row' style={{ padding: '10px' }}>
+                <div className='col-md-6' style={{ padding: "20px" }}>
+                    <div className='row' style={{ border: "3px solid red", padding: '10px' }}>
                         <h3>These are the tasks to be completed</h3>
-                    </div>
                     {
                         props.todos.length === 0 ? <div className='row' style={{padding:'10px'}}><center>No Todo's to display</center></div> :
                             props.todos.map((todo) => {
                                 return <TodoItem todo={todo} onDelete={props.onDelete} key={todo.sno} />
                             })
                     }
+                    </div>
                 </div>
             </div>
         </div>
